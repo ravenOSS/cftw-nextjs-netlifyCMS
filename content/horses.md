de36f1993989b794d8b1247ad1bc0f335c640355
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-03-22 12:12:41 -0600

    Next core site Add next.config.js Add horses.md content

---
+++ content/horses.md
@@ -0,0 +1,18 @@
+---
+title: Great Horses
+date: 2021-02-22T23:31:20:551Z
+horses:
+  - name: Excepzional
+    description: Excepzional a grey Polish-Crabbet Arabian stallion and an experienced competitor.
+    character: Intelligent, opinionated, FAST.
+  - name: Chanz
+    description: Chanz is a chestnut colored Arabian out of the Rushcreek line.
+    character: Great sense of humor, curious, removes bucket lids, easily spooked, challenge to stay in saddle.
+  - name: Aris
+    description: Aris is a grey Arabian and half brother to Chanz.
+    character: Earnest, cautious. Hell of a bucking horse (unfortunately!).
+---
+
+This is the profile page for some of my horses.
+
+Also, this features Next.js as a React.js framework
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,27 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import { attributes, react as HorseContent } from '../content/horses.md'
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	let { title, date, horses } = attributes
+	return (
+		<>
+			<Head>
+				<script src='https://identity.netlify.com/v1/netlify-identity-widget.js'></script>
+				<title>Horses</title>
+			</Head>
+			<article className='container'>
+				<h1>{title}</h1>
+				<h1>{date}</h1>
+				<HorseContent />
+				<ul>
+					{horses.map((horse, k) => (
+						<li key={k}>
+							<h2>{horse.name}</h2>
+							<p>{horse.description}</p>
+							<p>{horse.character}</p>
+						</li>
+					))}
+				</ul>
+			</article>
+		</>
+	)
 }
