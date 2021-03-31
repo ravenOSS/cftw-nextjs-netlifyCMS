22c8fc0215f579df255a2fe37a63adbe879f2841
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-03-31 15:40:04 -0600

    Revert date utility Add AppleHQ img

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { attributes, react as HorseContent } from '../content/horses.md'
-import Date from '../utilities/date'
+// import Date from '../utilities/date'
 export default function Home() {
 	let { title, date, horses } = attributes
 
@@ -18,7 +18,6 @@ export default function Home() {
 				<h1>{title}</h1>
 				{/* <p>{Date({ date })}</p> */}
 				<p>{date}</p>
-				<p>Date()</p>
 
 				<HorseContent />
 				<ul>
